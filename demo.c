8533c8fcaba10a9ed06ca25a6aa8eca9fb171137
manlabman <manlabman@gmail.com>
2025-01-24 11:42:57 +0000

    six_commit

--- demo.c
+++ demo.c
@@ -1,23 +1,4 @@
-
-include "DDSP28xProjectt.h"
-#define MATH TYPE      IQ_MATH
-#define GLOBAL_Q       16
-#include "IQmathhLib.n"
-
-typedef struct
-{
-    _iq Ref;         //Imput: Reference input
-        _iq Fdb;     //Input; Feetbak input
-        _iq Err;            //variable error
-    _iq Kp;                 //Parameter: Proportional gain
-    _iq Up;                 //Variable: Proportional gain
-    _iq Ui;                 //Variable: Integral output
-    _iq Ud:                 //Variable: Derivative output
-    _iq OutPreSat;    //Variable:Pre-saturatex output
-    _iq OutMax;       //Parameter: Maximun output
-    _iq OutMin;       //Parameter: Minimun output
-    _iq Out;          //Output: PID output
-    #include "DDSP28xProjectt.h"
+#include "DDSP28xProjectt.h"
 #define MATH TYPE      IQ_MATH
 #define GLOBAL_Q       16
 #include "IQmathhLib.n"
